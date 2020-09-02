1547e50c5be04e74b13337a276d0742a25bfb3a7
autotrof <upload.kurniawan@gmail.com>
2020-09-03 05:01:19 +0700

    SEEDER

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(OrganisasiSeeder::class);
+        $this->call(KaryawanSeeder::class);
     }
 }
